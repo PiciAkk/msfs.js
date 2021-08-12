723ed57be7df7271283632960b78b58d20b37a98
PiciAkk <marci@dbx.hu>
2021-08-12 17:44:10 +0200

    big changes, and stop command added

--- main.js
+++ main.js
@@ -15,7 +15,13 @@ lolcatjs.fromString('Activating MSFS.js...');
 var watcher = chokidar.watch(__dirname, {ignored: /^\./, persistent: true});
 
 watcher
-  .on('add', function(path) {console.log('File', path, 'has been added');})
-  .on('change', function(path) {console.log('File', path, 'has been changed');})
-  .on('unlink', function(path) {console.log('File', path, 'has been removed');})
-  .on('error', function(error) {console.error('Error happened', error);})
+  .on('add', function(path) { 
+   	if (path.includes("node_modules")) { 
+   		console.log("Failed installation detected..."); 
+   	} else if (path.includes("msfsKillSignal")) {
+   		lolcatjs.fromString("Deactivating MSFS.js...");
+   	}
+  })
+  //.on('change', function(path) {console.log('File', path, 'has been changed');})
+  //.on('unlink', function(path) {console.log('File', path, 'has been removed');})
+  //.on('error', function(error) {console.error('Error happened', error);})
