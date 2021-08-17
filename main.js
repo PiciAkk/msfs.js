feb3d87bd5487e0e2dc5c3018e09f024f4925319
PiciAkk <marci@dbx.hu>
2021-08-17 23:00:10 +0200

    big update

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 const chokidar = require('chokidar');
-const lolcatjs = require('lolcatjs');
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
-
-lolcatjs.options.seed = Math.round(Math.random() * 1000);
-
-lolcatjs.options.colors = true;
-lolcatjs.fromString('Activating MSFS.js...');
+console.log("Activating MSFS.js...")
 
 var watcher = chokidar.watch(__dirname, {ignored: /^\./, persistent: true});
 
 watcher
-  .on('add', function(path) { 
-   	if (path.includes("node_modules")) { 
-   		console.log("Failed installation detected..."); 
+  .on('add', function(path) {
+   	if (path.includes("node_modules")) {
+   		console.log("Failed installation detected...");
    	} else if (path.includes("msfsKillSignal")) {
-   		lolcatjs.fromString("Deactivating MSFS.js...");
+      console.log("Deactivating MSFS.js...")
    	}
   })
   //.on('change', function(path) {console.log('File', path, 'has been changed');})
